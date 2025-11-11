d4661b579773be2aa83caa3a17f1a9a8007fb9bb
Alex Guaman <kennypallchizaca@gmail.com>
2025-11-11 17:52:22 -0500

    nuevos metodos

--- src/app/features/ProyectosDosPages/services/proyectos-service.ts
+++ src/app/features/ProyectosDosPages/services/proyectos-service.ts
@@ -1,22 +1,54 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProyectosService {
+  private readonly STORAGE_KEY = 'proyectos_APP';
+  proyectos = signal<Proyecto[]>(this.loadProyectos());
 
-  constructor() { }
+  constructor() {
+    effect(() => {
+      const data = this.proyectos();
+      localStorage.setItem(this.STORAGE_KEY, JSON.stringify(data));
+    });
+  }
 
-  proyectos = signal<Proyecto[]>([
-    {
-      id: 1,
-      nombre: 'Proyecto 1',
-      descripcion: 'Descripción del Proyecto 1',
-    },
-  ]);
+  private loadProyectos(): Proyecto[] {
+    const data = localStorage.getItem(this.STORAGE_KEY);
+    return data
+      ? JSON.parse(data)
+      : [
+          {
+            id: 1,
+            nombre: 'Proyecto 1',
+            descripcion: 'Descripción del Proyecto 1',
+          },
+        ];
+  }
 
-
-  addProyecto(newProyecto : Proyecto) {
+  addProyecto(newProyecto: Proyecto) {
     this.proyectos.set([...this.proyectos(), newProyecto]);
   }
+
+
+  deleteProyecto(proyectoToDelete: Proyecto) {
+    const updatedProyectos = this.proyectos().filter(
+      (proyecto) => proyecto.id !== proyectoToDelete.id
+    );
+    this.proyectos.set(updatedProyectos);
+  }
+
+  removeallProyecto(proyectoToRemove: Proyecto) {
+    this.proyectos.set([]);
+  }
+
+
+  removewithname(name: string) {
+    const updatedProyectos = this.proyectos().filter(
+      (proyecto) => proyecto.nombre !== name
+    );
+    this.proyectos.set(updatedProyectos);
+  }
+
 }
--- src/app/features/ProyectosPages/components/add-component/add-proyecto/add-proyecto.ts
+++ src/app/features/ProyectosPages/components/add-component/add-proyecto/add-proyecto.ts
@@ -9,6 +9,10 @@ import { ChangeDetectionStrategy, Component, output, signal } from '@angular/cor
 export class AddProyecto {
   //outputs
   newProyecto = output<Proyecto>();
+  removeProyecto = output<Proyecto>();
+  remove1Proyecto = output<Proyecto>();
+  removewithnm = output<string>();
+
 
   name = signal('');
   description = signal('');
@@ -29,6 +33,36 @@ export class AddProyecto {
       descripcion: this.description(),
     };
     this.newProyecto.emit(newProyecto);
+    this.name.set('');
+    this.description.set('');
+  }
+
+  deleteProyecto() {
+    const proyectoToDelete: Proyecto = {
+      id: Math.floor(Math.random() * 10000),
+      nombre: this.name(),
+      descripcion: this.description(),
+    };
+    this.removeProyecto.emit(proyectoToDelete);
+    this.name.set('');
+    this.description.set('');
+  }
+
+
+  removeallProyecto() {
+    const proyectoToRemove: Proyecto = {
+      id: Math.floor(Math.random() * 10000),
+      nombre: this.name(),
+      descripcion: this.description(),
+    };
+    this.remove1Proyecto.emit(proyectoToRemove);
+    this.name.set('');
+    this.description.set('');
+  }
+  
+  removewithname() {
+    const nameToRemove = this.name();
+    this.removewithnm.emit(nameToRemove);
     this.name.set('');
     this.description.set('');
   }
